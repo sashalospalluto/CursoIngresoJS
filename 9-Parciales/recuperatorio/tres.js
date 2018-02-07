e2417ddb922fa3afe8cd0049ee8ce5f2b2b9364d
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-02-06 22:19:53 -0300

    recuperatorio3

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,22 @@
 function Mostrar()
 {
+	var precio1;
+	var precio2;
+	var precio3;
+	var suma;
+	var promedio;
+
+	precio1=document.getElementById('precioUno').value;
+	precio2=document.getElementById('precioDos').value;
+	precio3=document.getElementById('precioTres').value;
+
+	precio1=parseInt(precio1);
+	precio2=parseInt(precio2);
+	precio3=parseInt(precio3);
+
+	suma = precio1+precio2+precio3;
+	promedio= (precio1+precio2+precio3)/3;
+
+	alert ("La suma es "+suma+" y el promedio es "+promedio);
 
 }
