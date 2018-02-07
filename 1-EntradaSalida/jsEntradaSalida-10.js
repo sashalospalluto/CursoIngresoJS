c799729e15083b209dd3e1a5142d93fad5841987
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-02-06 22:15:36 -0300

    6,7,8,9,10
    nada

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,15 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe;
+	var resultadoDescuento;
+
+	importe = document.getElementById('importe').value;
+
+	importe = parseInt (importe);
+
+	resultadoDescuento = importe-(25*importe/100);
+
+	document.getElementById('resultado').value = resultadoDescuento;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,22 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo;
+	var sueldoConAumento;
 
+	sueldo = document.getElementById('sueldo').value;
+
+	sueldo= parseInt(sueldo);
+
+	sueldoConAumento = sueldo*1.1; 
+   
+
+   /*
+   sueldo + sueldo 10/100
+   sueldo (1+1/10)
+   sueldo*(1.1)
+   */
 	
+	document.getElementById('resultado').value =sueldoConAumento;
+
 }
