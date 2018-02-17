8052530e0a44e2edc28f0a421c570529bb34ff24
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-02-17 09:46:36 -0300

    IF 10
    Listo

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,26 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroRandom;
+
+	numeroRandom=Math.floor(Math.random() * 10) + 1;
+
+	if (numeroRandom>8)
+	{
+		alert ("Exelente "+numeroRandom);
+	} else
+	{
+		if (numeroRandom>3) 
+		{
+			alert ("Aprobo "+numeroRandom);
+
+		} else
+
+		{
+			alert ("Vamos, la proxima se puede "+numeroRandom)
+		}
+	}
+
+
 
 }//FIN DE LA FUNCIÓN
