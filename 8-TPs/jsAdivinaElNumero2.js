8bd43e9a497a8f48cdaa6bbb4056cc8d305d57a6
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-02-19 19:09:21 -0300

    tp6
    funciona

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -12,17 +12,76 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 var numeroSecreto; 
 var contadorIntentos;
 
+contadorIntentos=0
+
 function comenzar()
 {
+
 	//Genero el número RANDOM entre 1 y 100
+		numeroSecreto=Math.floor(Math.random() * 100)+1; 
 	 
+		//alert(numeroSecreto );
+
+		console.log (numeroSecreto);
 	
 
 }
 
 function verificar()
 {
-	
-	
+	var numeroIngresado;
+	var diferenciaNumero;
 
+	numeroIngresado=document.getElementById('numero').value;
+	contadorIntentos++;
+	document.getElementById('intentos').value=contadorIntentos;
+	
+	if (numeroIngresado==numeroSecreto)
+	{
+		switch (contadorIntentos)
+		{
+			case 1:
+				alert ("usted es un Psíquico");
+				break;
+				
+			case 2:
+				alert ("exelente percepcion");
+				break;
+
+			case 3:
+				alert ("Esto es suerte");
+				break;
+
+			case 4:
+				alert ("Exelente tecnica");
+				break;
+
+			case 5:
+				alert ("Usted esta en la media");
+				break;
+
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+				alert ("Falta Tecnica");
+
+			default:
+				alert ("Afortunado en el amor");
+				break;
+		}
+	}
+
+	else if (numeroIngresado>numeroSecreto)
+	{
+		diferenciaNumero = numeroIngresado-numeroSecreto;
+
+		alert ("Se paso por "+diferenciaNumero);
+	}	else
+		{
+			diferenciaNumero = numeroSecreto-numeroIngresado;
+
+		    alert ("Le falta "+diferenciaNumero);
+		}
 }
