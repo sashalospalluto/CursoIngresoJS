377c22ac3573d4573787e9dced882250bde0238c
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-02-19 20:21:55 -0300

    while 3 y 4
    funcionan

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,16 @@
-function Mostrar()
+	
+	function Mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+	var clave = prompt("ingrese el número clave.");
 
+	while (clave != "utn750")
+		{
+			alert ("clave incorrecta, vuelva a intentar");
+			clave = prompt("ingrese el número clave.");
+
+		}
+
+	alert ("Bienvenido");
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,17 @@
 function Mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero = prompt("ingrese un número entre 0 y 9.");
+
+	while (numero<0 || numero>9)
+		{
+			alert ("Vuelva a ingresar un numero valido");
+			numero = prompt("ingrese un número entre 0 y 9.");
+		}
+
+	document.getElementById('Numero').value=numero;
+
+
 
 
 }//FIN DE LA FUNCIÓN
