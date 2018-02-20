680baac159f429a1f0a3157eacda0736955489e2
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-02-19 21:54:17 -0300

    while 9
    funciona

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,40 @@ function Mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
 	var respuesta='si';
+	var numero;
+	var maximo=0;
+	var minimo=0;
 
-	while(respuesta!='no')
+	while(respuesta== "si")
 	{
-		
+		numero = prompt ("ingrese un numero");
+		numero = parseInt (numero);
+		contador++;
+		 
+		if (contador==1)
+		{
+		 	maximo=numero;
+		 	minimo=numero;
+		}
+
+		if (maximo<numero)
+		{
+			maximo=numero;
+		}	else
+			{	if (minimo>numero)
+				{
+					minimo=numero;
+				}
+			}
+
+		respuesta = prompt ("Indique con un 'si' si desea seguir ingresando un numero, de lo contrario ingrese un 'no'");
 	
 	}
 
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;
+
+}//FIN DE LA FUNCIÓN.
 
 
-
-}//FIN DE LA FUNCIÓN
