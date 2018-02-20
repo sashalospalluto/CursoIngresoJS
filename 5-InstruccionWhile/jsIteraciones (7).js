ccdb68afedf67834153dc5780a7fc61bf515fc8d
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-02-19 21:38:43 -0300

    while 7 y 8
    funcionan

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,6 +4,18 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+
+	while (respuesta == "si")
+	{
+		numero = prompt ("ingrese un numero");
+		numero = parseInt (numero);
+		acumulador = acumulador+numero;
+		contador++;
+		respuesta = prompt ("Indique con un 'si' si desea seguir ingresando un numero, de lo contrario ingrese un 'no'");
+	}
+
+
 
 
 document.getElementById('suma').value=acumulador;
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,26 @@ function Mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var acumulador=0;
 	var respuesta='si';
+	var numero;
+
+	while (respuesta == "si")
+	{
+		numero = prompt ("ingrese un numero");
+		numero = parseInt (numero);
+		contador++;
+		if (numero>0)
+		{
+			positivo=numero+positivo;
+
+		}	else
+			{
+				negativo= negativo * (numero);
+			}
+
+		respuesta = prompt ("Indique con un 'si' si desea seguir ingresando un numero, de lo contrario ingrese un 'no'");
+	}
 
 
 document.getElementById('suma').value=positivo;
