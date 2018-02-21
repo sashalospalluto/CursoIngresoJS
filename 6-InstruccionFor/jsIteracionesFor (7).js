cddf895ebd1a42247800c086949329f28b8fe0d9
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-02-20 22:12:28 -0300

    for 7
    funciona

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,26 @@
 function Mostrar()
 {
+	var contadorDiv=0
+	var numero=prompt ("ingrese numero");
+	numero=parseInt(numero);
 
+	for (anterior=2;anterior<numero;anterior++)
+	{
+		if (numero%anterior==0)
+		{
+			document.write("<br>"+anterior);
+			contadorDiv++;
+		}
+		if (anterior>(numero/2))
+		{
+			break;
+		}
+	}
 
+	if (contadorDiv==0) //para saber si es primo
+	{
+		document.write ("es primo");
+	}
 
 
 }//FIN DE LA FUNCIÓN
