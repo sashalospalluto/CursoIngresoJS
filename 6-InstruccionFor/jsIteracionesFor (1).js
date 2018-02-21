fc7860f0dcdc49287fc4bbac316600ce45f4ef17
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-02-20 21:11:52 -0300

    for 1 y 2
    funcionan

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,98 @@
 function Mostrar()
+
 {
+	var contadorDePositivos=0;
+	var contadorDeNegativos=0;
+	var contadorDeCeros=0;
+	var contadorDePar=0;
+	var contadorDeImpar=0;
+	var contador50=0;
+
+
+	for (contador=-100;contador<100;contador++)
+	{
+		if (contador>0)
+		{
+			contadorDePositivos++;
+		}	else
+			{
+				if (contador<0)
+				{
+					contadorDeNegativos++;
+				}	else
+					{
+						contadorDeCeros++;
+					}
+
+			}
+
+		if (contador%2==0 && contador!=0)
+		{
+			contadorDePar++;
+		}	else
+			{
+				if (contador!=0)
+				{
+					contadorDeImpar++;
+				}
+			}
+		switch (contador)
+		{
+			case 50:
+				contador50++;
+				break;
+		}
+
+
+	}
+
+
+
+	document.write ("<br>positivos: "+contadorDePositivos);
+	document.write ("<br>negativos: "+contadorDeNegativos);
+	document.write ("<br>ceros: "+contadorDeCeros);
+	document.write ("<br>pares: "+contadorDePar);
+	document.write ("<br>impares: "+contadorDeImpar);
+	document.write ("<br>contador de 50: "+contador50);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+{
+	var contador=0;
+	for (;;)
+	{
+		contador++;
+		document.write("<br>" +contador);
+		if (contador==10)
+		{
+			document.write("<br>break" +contador);
+			break;
+		}
+	
+		if (contador==5)
+		{
+		continue;
+		}
+		document.write ("<br>continue" +contador);
+	}
+	
+}
+*/
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,114 @@
 function Mostrar()
 {
+	var numeroRandom;
+	var contadorCero=0;
+	var contadorUno=0;
+	var contadorDos=0;
+	var contadorTres=0;
+	var contadorCuatro=0;
+	var contadorCinco=0;
+	var contadorSeis=0;
+	var contadorSiete=0;
+	var contadorOcho=0;
+	var contadorNueve=0;
+	var porcen0;
+	var porcen1;
+	var porcen2;
+	var porcen3;
+	var porcen4;
+	var porcen5;
+	var porcen6;
+	var porcen7;
+	var porcen8;
+	var porcen9;
 
 
+	for (contador=0;contador<100;contador++)
+	{
+		numeroRandom = Math.floor(Math.random() * 10);
+
+		switch (numeroRandom)
+		{
+			case 0:
+				contadorCero++;
+				break;
+			case 1:
+				contadorUno++;
+				break;
+
+			case 2:
+				contadorDos++;
+				break;
+
+			case 3:
+				contadorTres++;
+				break;
+
+			case 4:
+				contadorCuatro++;
+				break;
+
+			case 5:
+				contadorCinco++;
+				break;
+
+			case 6:
+				contadorSeis++;
+				break;
+
+			case 7:
+				contadorSiete++;
+				break;
+
+			case 8:
+				contadorOcho++;
+				break;
+
+			case 9:
+				contadorNueve++;
+				break;
+
+		}
+	}
+
+		porcen1= (contadorUno/(contador+1))*100;
+		porcen1=Math.round(porcen1); //sirve para redondear
+		document.write ("<br>Cantidad de 1: "+contadorUno+ "% :"+porcen1);
+
+		porcen2= (contadorDos/(contador+1))*100;
+		porcen2=Math.round(porcen2); //sirve para redondear
+		document.write ("<br>Cantidad de 2: "+contadorDos+ "% :"+porcen2);
+
+		porcen3= (contadorTres/(contador+1))*100;
+		porcen3=Math.round(porcen3); //sirve para redondear
+		document.write ("<br>Cantidad de 3: "+contadorTres+ "% :"+porcen3);
+
+		porcen4= (contadorCuatro/(contador+1))*100;
+		porcen4=Math.round(porcen4); //sirve para redondear
+		document.write ("<br>Cantidad de 4: "+contadorCuatro+ "% :"+porcen4);
+
+		porcen5= (contadorCinco/(contador+1))*100;
+		porcen5=Math.round(porcen5); //sirve para redondear
+		document.write ("<br>Cantidad de 5: "+contadorCinco+ "% :"+porcen5);
+
+		porcen6= (contadorSeis/(contador+1))*100;
+		porcen6=Math.round(porcen6); //sirve para redondear
+		document.write ("<br>Cantidad de 6: "+contadorSeis+ "% :"+porcen6);
+
+		porcen7= (contadorSiete/(contador+1))*100;
+		porcen7=Math.round(porcen7); //sirve para redondear
+		document.write ("<br>Cantidad de 7: "+contadorSiete+ "% :"+porcen7);
+
+		porcen8= (contadorOcho/(contador+1))*100;
+		porcen8=Math.round(porcen8); //sirve para redondear
+		document.write ("<br>Cantidad de 8: "+contadorOcho+ "% :"+porcen8);
+
+		porcen9= (contadorNueve/(contador+1))*100;
+		porcen9=Math.round(porcen9); //sirve para redondear
+		document.write ("<br>Cantidad de 9: "+contadorNueve+ "% :"+porcen9);
+		
+		porcen0= (contadorCero/(contador+1))*100;
+		porcen0=Math.round(porcen0); //sirve para redondear
+		document.write ("<br>Cantidad de 0: "+contadorCero+ "% :"+porcen0);
+	
 }
